7b28cc7cdb1364b0862e80df78fa250adf0a3d3c
Anurag Singh <anurag2307singh@gmail.com>
2025-07-07 18:06:37 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,26 @@
+
+import express from "express";
+import userRoutes from "./routes/userRouters.js"
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+const app = express();
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes);
+const server = createServer(app);
+const io = new Server(server);
+app.get("/home", (req, res) => {
+  return res.json({ "hello": "world" });
+});
+const start = async () => {
+  const connectionDb = await mongoose.connect("mongodb+srv://akbgs0001:7294985449aA@cluster0.91qscjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+  console.log(connectionDb.connection.host);
+  server.listen(app.get("port"), () => {
+    console.log("listening on port 8000 server");
+  })
+}
+start();
---
+++ Backend/src/routes/userRouters.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { login, register } from "../controllers/user.controller";
+const router = Router();
+router.route("/login").post(login);
+router.route("/register").post(register);
+export default router;
