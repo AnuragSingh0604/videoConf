f4b35ef7bf4a3685ff0a580a8763c55d97484419
Anurag Singh <anuragsingh@Anurags-MacBook-Air.local>
2025-08-08 12:47:16 +0530

    started frontend development

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/Landing";
+function App(){
+    return(<>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+    </>)
+}
+export default App;
