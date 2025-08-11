82b324ca10bb646b83ec463c1d54b4a8adcb9ff2
Anurag Singh <anurag2307singh@gmail.com>
2025-08-11 13:02:35 +0530

    created auth page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
+import Authentication from "./pages/auth";
 function App(){
     return(<>
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<Authentication/>}/>
       </Routes>
     </Router>
     </>)
--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import "../App.css";
+import { Link } from 'react-router-dom';
 export default function LandingPage(){
     return(
-        <div>Landing Page</div>
+        <div className='landingpageContainer'> 
+        <nav> 
+        <div className="navHeader">
+            <h2>QuickCall</h2>
+
+        </div>
+        <div className="navList">
+            <p>Join as Guest</p>
+            <p>Register</p>
+            <div role="button"><p>Login</p></div>
+        </div>
+            </nav>
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color:"#FF9839"}}>connect</span> with your loved ones</h1>
+                  <p>Cover a distance by QuickCall</p>
+                  <div role="button">
+                    <Link to="/home">Get Started</Link>
+                  </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt=""/>
+                </div>
+
+            </div>
+            </div>
     )
 }
